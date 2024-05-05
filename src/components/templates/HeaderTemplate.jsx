a0719e142aef597e8e69c3f1ac963d4002470da8
douglas <douglasmaxton@gmail.com>
2024-05-05 21:44:09 +0100

    created: a hamburger menu to show and hide when on mobile

--- src/components/templates/HeaderTemplate.jsx
+++ src/components/templates/HeaderTemplate.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
+// styling
+import styles from "../../styles/templates/HeaderTemplate.module.css";
+
 const HeaderTemplate = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = open ? (
+    <i className="fa-solid fa-xmark"></i>
+  ) : (
+    <i className="fa-solid fa-bars"></i>
+  );
+
   return (
     <header>
-      <h1>Header</h1>
+      <div className={styles.hamburger}>
+        <button onClick={() => setOpen(!open)}>{toggle}</button>
+        {open && <h1>Show</h1>}
+      </div>
     </header>
   );
 };
