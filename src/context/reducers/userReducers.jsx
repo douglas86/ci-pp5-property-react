9e6f4bb6db8b5bb7d8934d4ad25c8dc258b218e1
douglas <douglasmaxton@gmail.com>
2024-05-05 21:46:33 +0100

    added: docstring for user reducer function

--- src/context/reducers/userReducers.jsx
+++ src/context/reducers/userReducers.jsx
@@ -1,3 +1,9 @@
+/**
+ * This reducer function will handle all data for current logged-in user
+ * @param state
+ * @param action
+ * @returns {*|(*&{user})}
+ */
 export const userReducers = (state, action) => {
   const { type, payload } = action;
 
