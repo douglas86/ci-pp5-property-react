d1f09dd69c8952ce2ef6ad9b180d3b1f1048ca0e
douglas <douglasmaxton@gmail.com>
2024-05-05 17:03:38 +0100

    added: docstring to LayoutTemplate.jsx

--- src/components/templates/LayoutTemplate.jsx
+++ src/components/templates/LayoutTemplate.jsx
@@ -1,3 +1,9 @@
+/**
+ * This template will be used to render Header, Footer and all content in between
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const LayoutTemplate = ({ children }) => {
   return <div>{children}</div>;
 };
