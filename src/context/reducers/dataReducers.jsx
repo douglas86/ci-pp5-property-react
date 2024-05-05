550ef92ff2fc20ad298c5a9f3cf2002a49454058
douglas <douglasmaxton@gmail.com>
2024-05-05 21:44:44 +0100

    added: docstring for dataReducer function

--- src/context/reducers/dataReducers.jsx
+++ src/context/reducers/dataReducers.jsx
@@ -1,3 +1,9 @@
+/**
+ * This reducer function is used to deal with data from API
+ * @param state
+ * @param action
+ * @returns {(*&{data})|*}
+ */
 export const dataReducers = (state, action) => {
   const { type, payload } = action;
 
