62c19dfdffdd9d7d8971e77259142b0f1f5af837
douglas <douglasmaxton@gmail.com>
2024-05-06 17:01:37 +0100

    created: sign in page with its path

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,7 +1,8 @@
+// pages
 import HomePage from "../components/pages/HomePage";
 import AboutPage from "../components/pages/AboutPage";
 import DashboardPage from "../components/pages/DashboardPage";
-import ContactPage from "../components/pages/ContactPage";
+import SignInPage from "../components/pages/auth/SignInPage";
 import NotFoundPage from "../components/pages/NotFoundPage";
 
 export const router = [
@@ -12,22 +13,22 @@ export const router = [
     path: "/",
   },
   {
-    title: "About",
-    icon: "",
-    component: <AboutPage />,
-    path: "/about",
-  },
-  {
     title: "Dashboard",
     icon: "",
     component: <DashboardPage />,
     path: "/dashboard",
   },
   {
-    title: "Contact",
+    title: "About",
     icon: "",
-    component: <ContactPage />,
-    path: "/contact",
+    component: <AboutPage />,
+    path: "/about",
+  },
+  {
+    title: "Sign In",
+    icon: "",
+    component: <SignInPage />,
+    path: "/sign-in",
   },
   {
     title: "None",
