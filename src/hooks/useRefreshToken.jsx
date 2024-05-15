1422fe4d5600f90fe8fc90ee2537a9994d96f8e8
douglas <douglasmaxton@gmail.com>
2024-05-15 16:46:17 +0100

    saved: access and refresh token to state store

--- src/hooks/useRefreshToken.jsx
+++ src/hooks/useRefreshToken.jsx
@@ -10,15 +10,19 @@ const useRefreshToken = () => {
 
   const handleMount = async () => {
     try {
-      return await axiosRes.get("dj-rest-auth/user/");
+      return await axios.get("dj-rest-auth/user/");
     } catch (err) {
-      setRefreshToken(null);
+      dispatch({ type: "UPDATE USER DATA", payload: null });
     }
   };
 
   useEffect(() => {
     handleMount()
-      .then(({ data }) => dispatch({ type: "UPDATE USER DATA", payload: data }))
+      .then((data) =>
+        data
+          ? dispatch({ type: "UPDATE USER DATA", payload: null })
+          : dispatch({ type: "UPDATE FORM STATE", payload: data.data.user }),
+      )
       .catch((err) => dispatch({ type: "UPDATE USER DATA", payload: null }));
   }, []);
 
@@ -29,13 +33,6 @@ const useRefreshToken = () => {
           console.log("error9");
           await axios.post("/dj-rest-auth/token/refresh/");
         } catch (err) {
-          setRefreshToken((prevCurrentUser) => {
-            if (prevCurrentUser) {
-              console.log("You are here");
-            }
-            console.log("error5");
-            return null;
-          });
           console.log("error6");
           return config;
         }
@@ -49,33 +46,31 @@ const useRefreshToken = () => {
     );
 
     axiosRes.interceptors.response.use(
-      (response) => response,
+      (response) => dispatch({ type: "UPDATE USER DATA", payload: response }),
       async (err) => {
         if (err.response?.status === 401) {
-          try {
-            await axios.post("/dj-rest-auth/token/refresh/");
-          } catch (error) {
-            setRefreshToken((prevCurrentUser) => {
-              if (prevCurrentUser) {
-                console.log("Another detour");
-              }
-              console.log("error");
-              return null;
-            });
-          }
-          console.log("error2");
-          // return axios(err.config);
+          dispatch({ type: "UPDATE USER DATA", payload: null });
         }
-        console.log("error3");
+        //   try {
+        //     await axios.post("/dj-rest-auth/token/refresh/");
+        //   } catch (error) {
+        //     setRefreshToken((prevCurrentUser) => {
+        //       if (prevCurrentUser) {
+        //         console.log("Another detour");
+        //       }
+        //       console.log("error");
+        //       return null;
+        //     });
+        //   }
+        //   console.log("error2");
+        //   // return axios(err.config);
+        // }
         return Promise.reject(err);
       },
     );
-  }, []);
+  }, [dispatch]);
 
   console.log("refreshToken", refreshToken);
-
-  console.log("error4");
-  return refreshToken;
 };
 
 export default useRefreshToken;
