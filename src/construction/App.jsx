8bb8cfda1ed2082b25fbebe5078cfaef8b71cc90
douglas <douglasmaxton@gmail.com>
2024-05-16 13:08:08 +0100

    added: useEffect hook to hide alert after 5 seconds

--- src/construction/App.jsx
+++ src/construction/App.jsx
@@ -13,11 +13,27 @@ import { router } from "../utils";
 // styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import useRefreshToken from "../hooks/useRefreshToken";
+import useAppContext from "../hooks/useAppContext";
+import { useEffect } from "react";
 
 const App = () => {
+  const { state, dispatch } = useAppContext();
+  const { dataReducers } = state;
+  const { showAlert } = dataReducers;
+
+  // custom hook that detects page width
   useResize();
 
+  // custom hook that refreshes the auth tokens
   useRefreshToken();
+
+  // useEffect hook to hide the show alert message after 5 seconds
+  useEffect(() => {
+    showAlert &&
+      setTimeout(() => {
+        dispatch({ type: "HIDE ALERT MESSAGE" });
+      }, 5000);
+  }, [showAlert]);
 
   return (
     <LayoutTemplate>
--- src/context/reducers/dataReducers.jsx
+++ src/context/reducers/dataReducers.jsx
@@ -8,8 +8,10 @@ export const dataReducers = (state, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case "TOGGLE ALERT MESSAGE":
+    case "SHOW ALERT MESSAGE":
       return { ...state, alertMessage: payload, showAlert: true };
+    case "HIDE ALERT MESSAGE":
+      return { ...state, showAlert: false };
     case "SET_DATA":
       return { ...state, data: payload };
     default:
