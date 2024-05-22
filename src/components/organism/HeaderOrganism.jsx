8766eab7ef9e315a40e929104b9032a1297b1bbe
douglas <douglasmaxton@gmail.com>
2024-05-22 15:17:21 +0100

    deleted: all unwanted and commented out code

--- src/components/organism/HeaderOrganism.jsx
+++ src/components/organism/HeaderOrganism.jsx
@@ -1,11 +1,18 @@
+// 3rd party libraries
+import { useEffect } from "react";
+
+// components
+import LinksMolecule from "../molecule/LinksMolecule";
+import MenuMolecule from "../molecule/MenuMolecule";
+
+// custom hooks
+import useAppContext from "../../hooks/useAppContext";
+
+// assets
 import logo from "../../assets/images/header/Property.png";
 
 // styles
 import styles from "../../styles/organism/HeaderOrganism.module.css";
-import useAppContext from "../../hooks/useAppContext";
-import { useEffect } from "react";
-import LinksMolecule from "../molecule/LinksMolecule";
-import MenuMolecule from "../molecule/MenuMolecule";
 
 /**
  * This is a higher order component that is responsible for the header
@@ -40,48 +47,3 @@ const HeaderOrganism = () => {
 };
 
 export default HeaderOrganism;
-
-// // 3rd party libraries
-// import { useEffect } from "react";
-//
-// // components
-// import LinksMolecule from "../molecule/LinksMolecule";
-// import MenuMolecule from "../molecule/MenuMolecule";
-// import { logoIcon } from "../atom/Icons";
-//
-// // custom hooks
-// import useAppContext from "../../hooks/useAppContext";
-//
-// // assets
-// import logo from "../../assets/images/house.png";
-//
-// // styling
-// import styles from "../../styles/organism/HeaderOrganism.module.css";
-//
-// const HeaderOrganism = () => {
-//   const { state, dispatch } = useAppContext();
-//   const { stateReducers } = state;
-//   const { MenuOpen, width } = stateReducers;
-//
-//   useEffect(() => {
-//     width >= 768 &&
-//       dispatch({ type: "UPDATE HAMBURGER TOGGLE", payload: false });
-//   }, [width, dispatch]);
-//
-//   return (
-//     <header className={styles.header}>
-//       <nav className={styles.nav}>
-//         <div className={styles.title}>
-//           <div className={styles.logo}>{logoIcon(logo)}</div>
-//           <h1 className={styles.h1}>London Properties</h1>
-//         </div>
-//         <div className={styles.links}>
-//           {width > 768 ? <LinksMolecule /> : <MenuMolecule />}
-//         </div>
-//       </nav>
-//       <div className={styles.menu}>{MenuOpen && <LinksMolecule />}</div>
-//     </header>
-//   );
-// };
-//
-// export default HeaderOrganism;
