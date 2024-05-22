52210aea8a79b9851ef525f6a82e09b68a6bb4ec
douglas <douglasmaxton@gmail.com>
2024-05-22 17:19:00 +0100

    created: a carousel organism component

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,8 +1,14 @@
 // components
 import PageTitleMolecule from "../molecule/PageTitleMolecule";
+import CarouselOrganism from "../organism/CarouselOrganism";
 
 const LandingPage = () => {
-  return <PageTitleMolecule />;
+  return (
+    <>
+      <PageTitleMolecule />
+      <CarouselOrganism />
+    </>
+  );
 };
 
 export default LandingPage;
