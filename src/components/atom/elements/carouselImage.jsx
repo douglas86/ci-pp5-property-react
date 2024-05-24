c6148f5840be8d531439fe326b7bbf1808af74e5
douglas <douglasmaxton@gmail.com>
2024-05-24 10:52:41 +0100

    added: doc string to carouselImage.jsx

--- src/components/atom/elements/carouselImage.jsx
+++ src/components/atom/elements/carouselImage.jsx
@@ -1,5 +1,11 @@
 import styles from "../../../styles/atom/elements.module.css";
 
+/**
+ * Displaying of carousel images with animation
+ * @param image - image to be displayed
+ * @param resetAnimation - this is a flag that will reset animation on button click
+ * @returns {JSX.Element}
+ */
 export const carouselImage = (image, resetAnimation) => (
   <img
     className={resetAnimation ? "" : `${styles.image} ${styles.animation}`}
