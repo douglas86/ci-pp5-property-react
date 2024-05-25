9448beba01dd1b4081e7225fc5232258d34b2669
douglas <douglasmaxton@gmail.com>
2024-05-25 16:33:26 +0100

    fixed: subheading to be center of its container

--- src/components/organism/TestimonialsOrganism.jsx
+++ src/components/organism/TestimonialsOrganism.jsx
@@ -1,10 +1,12 @@
 import { heading } from "../atom/elements";
 import CardMolecule from "../molecule/CardMolecule";
+import { subheading } from "../atom/elements/subheading";
 
 const TestimonialsOrganism = () => {
   return (
     <div>
       {heading("Testimonials")}
+      {subheading("Have a look what previous customers are saying")}
       <CardMolecule />
     </div>
   );
