c34da80e176db482e29e0d93dd732970694c2ffd
douglas <douglasmaxton@gmail.com>
2024-05-25 16:04:34 +0100

    created: user testimonial card

--- src/components/molecule/CardMolecule.jsx
+++ src/components/molecule/CardMolecule.jsx
@@ -1,8 +1,28 @@
-import styles from "../../styles/molecule/Card.module.css";
+// components
 import { subheading } from "../atom/elements/subheading";
 
+// assets
+import avatar from "../../assets/images/avatar/Avatar.png";
+
+// styling
+import styles from "../../styles/molecule/Card.module.css";
+
 const CardMolecule = () => {
-  return <div className={styles.card}>{subheading('"A terrific praise"')}</div>;
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardSubheading}>
+        {subheading('"A terrific praise"')}
+      </div>
+      <div className={styles.cardText}>
+        <div className={styles.thumbnail}>
+          <img src={`${avatar}`} alt="avatar" />
+        </div>
+        <div className={styles.description}>
+          <h4>Douglas Maxton</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardMolecule;
