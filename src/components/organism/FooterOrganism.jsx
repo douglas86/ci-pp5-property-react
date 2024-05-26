6f506965aac6e95ca26dc87c8296cb1558082a3d
douglas <douglasmaxton@gmail.com>
2024-05-26 12:38:39 +0100

    added: styling for thumbnails with center align

--- src/components/organism/FooterOrganism.jsx
+++ src/components/organism/FooterOrganism.jsx
@@ -4,11 +4,19 @@ import { h4Heading, thumbnail } from "../atom/elements";
 
 import emailIcon from "../../assets/images/footer/Email.png";
 import androidIcon from "../../assets/images/footer/Android Phone.png";
+import githubIcon from "../../assets/images/footer/GitHub.png";
+import LinkedinIcon from "../../assets/images/footer/LinkedIn Circled.png";
 
 const FooterOrganism = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.socialLinks}></div>
+      <div className={styles.socialLinks}>
+        {h4Heading("London Properties")}
+        <div className={styles.thumbnails}>
+          {thumbnail(githubIcon, "github icon")}
+          {thumbnail(LinkedinIcon, "linkedin icon")}
+        </div>
+      </div>
       <div className={styles.contactInformation}>
         <div className={styles.section}>
           {thumbnail(emailIcon, "email icon")}
