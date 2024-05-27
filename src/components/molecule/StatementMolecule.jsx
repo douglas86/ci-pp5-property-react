d185cb9ae76d419b881b4681dc7db2b07e79cb8e
douglas <douglasmaxton@gmail.com>
2024-05-27 13:27:00 +0100

    added: docstring to StatementMolecule.jsx

--- src/components/molecule/StatementMolecule.jsx
+++ src/components/molecule/StatementMolecule.jsx
@@ -4,6 +4,14 @@ import { heading, subheading } from "../atom/elements";
 // styling
 import styles from "../../styles/molecule/Statement.module.css";
 
+/**
+ * This molecule is used for making statements like about, mission or commitment statements
+ * @param title - title of the statement
+ * @param subHead - optional description of statement
+ * @param paragraph - this is the paragraph of the statement
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const StatementMolecule = ({ title, subHead, paragraph }) => {
   return (
     <div className={styles.section}>
