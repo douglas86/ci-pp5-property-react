301f7c98948d561cc2c7307c124b77694e46ec3a
douglas <douglasmaxton@gmail.com>
2024-05-27 13:19:19 +0100

    changed: mission and commitment to be an array for statement molecule

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -2,13 +2,28 @@
 import PageTitleMolecule from "../molecule/PageTitleMolecule";
 
 import styles from "../../styles/pages/about.module.css";
-import { subheading } from "../atom/elements/subheading";
-import { heading } from "../atom/elements";
 
 import house from "../../assets/images/about page/house.png";
 import StatementMolecule from "../molecule/StatementMolecule";
 
 const AboutPage = () => {
+  const missionCommitment = [
+    {
+      title: "Our Mission",
+      paragraph:
+        "Our mission is to help empower you with the tools, information and\n" +
+        "            support that you need to help you with achieving your vision to\n" +
+        "            obtain your next property.",
+    },
+    {
+      title: "Our Commitment",
+      paragraph:
+        "We are committed to transparency, integrity and honesty in\n" +
+        "            everything we do. Our goal is to build long lasting relationships\n" +
+        "            with our clients. Your satisfaction is our number 1 priority",
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <PageTitleMolecule />
@@ -23,22 +38,9 @@ const AboutPage = () => {
         <img className={styles.image} src={`${house}`} alt="house" />{" "}
       </div>
       <div className={styles.section}>
-        <div className={styles.about}>
-          {heading("Our Mission")}
-          <p>
-            Our mission is to help empower you with the tools, information and
-            support that you need to help you with achieving your vision to
-            obtain your next property.
-          </p>
-        </div>
-        <div className={styles.about}>
-          {heading("Our Commitment")}
-          <p>
-            We are committed to transparency, integrity and honesty in
-            everything we do. Our goal is to build long lasting relationships
-            with our clients. Your satisfaction is our number 1 priority
-          </p>
-        </div>
+        {missionCommitment.map(({ title, paragraph }) => (
+          <StatementMolecule title={title} paragraph={paragraph} />
+        ))}
       </div>
     </div>
   );
