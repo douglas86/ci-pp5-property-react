2b608076d28eda39ecdcdd59e22c7c2c5ac9c7de
douglas <douglasmaxton@gmail.com>
2024-05-30 15:55:05 +0100

    added: docstring to thumbnail atom

--- src/components/atom/elements/thumbnail.jsx
+++ src/components/atom/elements/thumbnail.jsx
@@ -1,5 +1,12 @@
+// styling
 import styles from "../../../styles/atom/elements.module.css";
 
+/**
+ * Atom is used to display thumbnail images
+ * @param src
+ * @param alt
+ * @returns {JSX.Element}
+ */
 export const thumbnail = (src, alt) => (
   <img className={styles.thumbnail} src={src} alt={alt} />
 );
