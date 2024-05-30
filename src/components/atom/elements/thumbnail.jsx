6d0832f2ed6e7816409e3df38a8575bdf48e5629
douglas <douglasmaxton@gmail.com>
2024-05-30 15:50:26 +0100

    changed: a media query from max-width 350 to 400 and adjusted thumbnail image

--- src/components/atom/elements/thumbnail.jsx
+++ src/components/atom/elements/thumbnail.jsx
@@ -1,1 +1,5 @@
-export const thumbnail = (src, alt) => <img src={src} alt={alt} />;
+import styles from "../../../styles/atom/elements.module.css";
+
+export const thumbnail = (src, alt) => (
+  <img className={styles.thumbnail} src={src} alt={alt} />
+);
