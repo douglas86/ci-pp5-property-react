2833273bc36c9660115f13f9c51dff93628a8e69
douglas <douglasmaxton@gmail.com>
2024-05-31 15:54:35 +0100

    created: two admin dashboard pages

---
+++ src/components/pages/dashboard/AdminPropertyPage.jsx
@@ -0,0 +1,9 @@
+const AdminPropertyPage = () => {
+  return (
+    <div>
+      <h1>Admin Property</h1>
+    </div>
+  );
+};
+
+export default AdminPropertyPage;
---
+++ src/components/pages/dashboard/AdminUsersPages.jsx
@@ -0,0 +1,9 @@
+const AdminUsersPages = () => {
+  return (
+    <div>
+      <h1>Admin Users</h1>
+    </div>
+  );
+};
+
+export default AdminUsersPages;
