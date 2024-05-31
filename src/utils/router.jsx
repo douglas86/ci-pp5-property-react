65e415621c27a861679bccf0a1db5612a17f05cf
douglas <douglasmaxton@gmail.com>
2024-05-31 16:03:42 +0100

    added: docstring and descriptive comments of an object for router.jsx

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -3,6 +3,8 @@ import LandingPage from "../components/pages/LandingPage";
 import AboutPage from "../components/pages/AboutPage";
 import DashboardPage from "../components/pages/DashboardPage";
 import NotFoundPage from "../components/pages/NotFoundPage";
+import AdminUsersPages from "../components/pages/dashboard/AdminUsersPages";
+import AdminPropertyPage from "../components/pages/dashboard/AdminPropertyPage";
 
 // components
 import { fontawesomeIcon } from "../components/atom/Icons";
@@ -11,9 +13,17 @@ import { fontawesomeIcon } from "../components/atom/Icons";
 import Home from "../assets/images/Home.png";
 import Dashboard from "../assets/images/Dashboard.png";
 import Info from "../assets/images/Info.png";
-import AdminUsersPages from "../components/pages/dashboard/AdminUsersPages";
-import AdminPropertyPage from "../components/pages/dashboard/AdminPropertyPage";
 
+// description of the keys within the object
+// title - this is the name on the navbar header
+// icon - the icon to be seen on the navbar - not in use yet
+// component - the pages that I want to load
+// path - of were the route will go
+
+/**
+ * This helper file is used for all page routes
+ * @type {[{path: string, component: JSX.Element, icon: *, title: string},{path: string, component: JSX.Element, icon: *, title: string},{path: string, component: JSX.Element, icon: *, title: string},{path: string, component: JSX.Element, icon: string, title: string},{path: string, component: JSX.Element, icon: string, title: string},null]}
+ */
 export const router = [
   // links on Navbar
   {
@@ -34,7 +44,7 @@ export const router = [
     component: <AboutPage />,
     path: "/about",
   },
-  // dashboard pages
+  // admin dashboard pages
   {
     title: "None",
     icon: "",
