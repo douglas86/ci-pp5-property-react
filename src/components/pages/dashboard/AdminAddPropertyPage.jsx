c78569f2a9d69c5148730d63abc8fade1db5106e
douglas <douglasmaxton@gmail.com>
2024-06-03 15:55:31 +0100

    added: a data array to pass to form organism

--- src/components/pages/dashboard/AdminAddPropertyPage.jsx
+++ src/components/pages/dashboard/AdminAddPropertyPage.jsx
@@ -1,11 +1,77 @@
 import AdminButtonsMolecule from "../../molecule/AdminButtonsMolecule";
+import Form from "react-bootstrap/Form";
 
 const AdminAddPropertyPage = () => {
+  const data = [
+    {
+      label: "Property Address",
+      type: "text",
+      placeholder: "Add new address here",
+      name: "property_address",
+    },
+    {
+      label: "Property Area",
+      type: "text",
+      placeholder: "Add property area",
+      name: "property_area",
+    },
+    {
+      label: "Postcode",
+      type: "text",
+      placeholder: "Add property postcode",
+      name: "area_code",
+    },
+    {
+      label: "Monthly Rent",
+      type: "number",
+      placeholder: "Add property monthly rent",
+      name: "rent",
+    },
+    {
+      label: "Property Image",
+      type: "file",
+      placeholder: "Add property image",
+      name: "property_image",
+    },
+  ];
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    console.log("e", e);
+  };
+
   return (
-    <div>
+    <>
       <AdminButtonsMolecule />
-      <h1>Add New Property</h1>
-    </div>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="file"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+          <Form.Label>Property Address</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+          <Form.Label>Property Area</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
+          <Form.Label>Area Code</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
+          <Form.Label>Rent</Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+      </Form>
+    </>
   );
 };
 
