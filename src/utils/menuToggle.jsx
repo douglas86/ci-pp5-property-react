830ed23dccc063c5cbc548bb6f395e26d2d1359a
douglas <douglasmaxton@gmail.com>
2024-06-14 09:49:17 +0100

    added: docstring to menuToggle.jsx

--- src/utils/menuToggle.jsx
+++ src/utils/menuToggle.jsx
@@ -1,5 +1,10 @@
 import { fontawesomeIcon } from "../components/atom/Icons";
 
+/**
+ * Toggles open and close of a hamburger menu on smaller devices
+ * @param menuState
+ * @returns {*}
+ */
 export const menuToggle = (menuState) =>
   menuState
     ? fontawesomeIcon("fa-solid fa-xmark")
