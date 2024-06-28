19ea4e1e20eca10fba2c397ce6dcd78d7fb29073
douglas <douglasmaxton@gmail.com>
2024-06-28 10:48:32 +0100

    fix: fetching data from API

--- src/components/pages/dashboard/admin/AdminPropertyPage.jsx
+++ src/components/pages/dashboard/admin/AdminPropertyPage.jsx
@@ -5,8 +5,29 @@ import IsAdmin from "../../../templates/Authentication/IsAdmin";
 
 // styling
 import styles from "../../../../styles/pages/admin.module.css";
+import { useEffect, useState } from "react";
+import useResize from "../../../../hooks/useResize";
+import AxiosInstance from "../../../../API/AxiosInstance";
 
 const AdminPropertyPage = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [body, setBody] = useState(null);
+
+  const width = useResize();
+
+  const headers = ["", "Property Owner", "Address", "Postcode", "Role", "Rent"];
+
+  useEffect(() => {
+    AxiosInstance.get("stocks/")
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }, []);
+
   return (
     <IsAdmin>
       <AdminButtonsMolecule />
