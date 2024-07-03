8838b8d612676ab50d579cd2493a293cc2dea727
douglas <douglasmaxton@gmail.com>
2024-07-03 05:28:39 +0100

    feat: replaced localhost with heroku for API calling

--- src/API/AxiosInstance.jsx
+++ src/API/AxiosInstance.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const localhost = "http://localhost:8000";
 const heroku = "https://ci-pp5-property-api-d3a66dbd3b1c.herokuapp.com";
 const access = Cookies.get("auth-token");
 const csrf = Cookies.get("csrftoken");
