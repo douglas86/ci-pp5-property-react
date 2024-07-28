e4f525e5b8ad88fc17bbb496c8e244cd774beb30
douglas <douglasmaxton@gmail.com>
2024-07-28 16:26:47 +0100

    feat: place .com back into heroku link

--- src/API/AxiosInstance.jsx
+++ src/API/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const heroku = "https://ci-pp5-property-api-958077e8a5b4.herokuapp.";
+const heroku = "https://ci-pp5-property-api-958077e8a5b4.herokuapp.com";
 const access = Cookies.get("auth-token");
 const csrf = Cookies.get("csrftoken");
 const refresh = Cookies.get("refresh-token");
