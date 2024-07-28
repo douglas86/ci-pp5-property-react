a60a43db6a2e8e39aca0e374bdbbb7a05d10bce5
douglas <douglasmaxton@gmail.com>
2024-07-28 18:24:12 +0100

    feat: deleted all commented out and dead code from registration form

--- src/components/organism/Forms/RegistrationForm.jsx
+++ src/components/organism/Forms/RegistrationForm.jsx
@@ -74,21 +74,9 @@ const RegistrationForm = () => {
       <div className={styles.buttons}>
         {buttonClick(
           async () => {
-            // await axios
-            //   .post(
-            //     "https://ci-pp5-property-api-958077e8a5b4.herokuapp.com/dj-rest-auth/registration/",
-            //     form,
-            //   )
-            //   .then(async (res) => {
-            //     console.log("res", res);
-            //   })
-            //   .catch((err) => {
-            //     console.log("err", err);
-            //   });
             await AxiosRegister.post("/dj-rest-auth/registration/", form)
               .then(async (res) => {
                 const results = await res.data.user;
-                console.log("results", results);
                 const { pk } = results;
                 dispatch({ type: "TOGGLE HIDE MODAL" });
                 dispatch({ type: "UPDATE USER DATA", payload: results });
@@ -96,22 +84,8 @@ const RegistrationForm = () => {
                 getProfileData(pk, dispatch);
               })
               .catch((err) => {
-                console.log("err", err);
                 dispatch({ type: "ERROR UPDATING USER DATA", payload: err });
               });
-            // await AxiosInstance.post("/dj-rest-auth/registration/", form)
-            //   .then(async (res) => {
-            //     const results = await res.data.user;
-            //     const { pk } = results;
-            //     dispatch({ type: "TOGGLE HIDE MODAL" });
-            //     dispatch({ type: "UPDATE USER DATA", payload: results });
-            //
-            //     getProfileData(pk, dispatch);
-            //   })
-            //   .catch((err) => {
-            //     console.log("err", err);
-            //     dispatch({ type: "ERROR UPDATING USER DATA", payload: err });
-            //   });
           },
           "Register",
           "success",
