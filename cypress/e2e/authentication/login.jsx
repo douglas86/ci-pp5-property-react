5b281a43c8969b9fbdce8d6435868e99c61b5324
douglas <douglasmaxton@gmail.com>
2024-07-31 18:20:44 +0100

    feat: create a test for token and login

--- cypress/e2e/authentication/login.jsx
+++ cypress/e2e/authentication/login.jsx
@@ -1,10 +1,10 @@
-import { local, server } from "../credentials";
+import { local, heroku } from "../credentials";
 
 export const login = () =>
   it("should be able to login", () => {
     cy.visit(`${local}`);
 
-    const username = "douglas";
+    const username = "ju";
     const password = "IAMininGLOrN";
 
     cy.get(".btn")
@@ -20,15 +20,20 @@ export const login = () =>
             .contains("Login")
             .click({ force: true })
             .then(() => {
-              cy.request({
-                method: "POST",
-                url: `${server}/dj-rest-auth/login/`,
-                body: { username, password },
-                failOnStatusCode: false,
-              }).then((resp) => {
-                const user = resp.body.user.username;
-                expect(user).to.equal(username);
+              cy.request("POST", `${heroku}/dj-rest-auth/login/`, {
+                username,
+                password,
               });
+              // cy.request({
+              //   method: "POST",
+              //   url: `${heroku}dj-rest-auth/login/`,
+              //   body: { username, password },
+              //   failOnStatusCode: false,
+              // }).then((resp) => {
+              //   console.log("resp", resp);
+              //   // const user = resp.body.user.username;
+              //   // expect(user).to.equal(username);
+              // });
             });
         });
       });
--- cypress/e2e/credentials.jsx
+++ cypress/e2e/credentials.jsx
@@ -1,3 +1,5 @@
-export const local = "http://localhost:3001";
+export const reactHeroku =
+  "https://ci-pp5-property-react-1c4b35a4e2b5.herokuapp.com/";
+export const local = "http://localhost:3000";
 export const server = "http://localhost:8000";
-export const heroku = "https://ci-pp5-property-api-d3a66dbd3b1c.herokuapp.com";
+export const heroku = "https://ci-pp5-property-api-958077e8a5b4.herokuapp.com/";
