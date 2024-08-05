3e2f7e0b135cabb021331b2926cf656bd6810112
douglas <douglasmaxton@gmail.com>
2024-08-05 17:14:18 +0100

    feat: added docstring to user reducer

--- src/context/reducers/userReducers.jsx
+++ src/context/reducers/userReducers.jsx
@@ -1,3 +1,9 @@
+/**
+ * This reducer functions handles all state for the current user
+ * @param state
+ * @param action
+ * @returns {{error}|{}|{user: (*)}|{profile: (*)}}
+ */
 export const userReducers = (state = {}, action) => {
   const { type, payload } = action;
 
@@ -14,46 +20,3 @@ export const userReducers = (state = {}, action) => {
       return state;
   }
 };
-
-// /**
-//  * This reducer function will handle all data for current logged-in user
-//  * @param state
-//  * @param action
-//  * @returns {*|(*&{user})}
-//  */
-// export const userReducers = (state, action) => {
-//   const { type, payload } = action;
-//
-//   switch (type) {
-//     case "UPDATE USER DATA":
-//       return {
-//         ...state,
-//         user: { ...state.user, ...payload },
-//       };
-//     case "UPDATE PROFILE DATA":
-//       return {
-//         ...state,
-//         profile: { ...state.profile, ...payload },
-//       };
-//     case "LOGIN USER":
-//       const { access, access_expiration, user } = payload;
-//       return {
-//         ...state,
-//         access,
-//         access_expiration,
-//         user: user === undefined ? state.user : user,
-//       };
-//     case "LOGOUT USER":
-//       return {
-//         ...state,
-//         access: null,
-//         access_expiration: null,
-//         user: null,
-//         profile: null,
-//       };
-//     case "ERROR UPDATING USER DATA":
-//       return { ...state, error: payload };
-//     default:
-//       return state;
-//   }
-// };
