9a71ad6e06c1e46f29416690581d9b00885aa6c7
douglas <douglasmaxton@gmail.com>
2024-08-09 16:18:45 +0100

    feat: import state to landing page

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,12 +1,19 @@
 import CarouselOrganism from "../organism/CarouselOrganism";
 import TestimonialsOrganism from "../organism/TestimonialsOrganism";
 import PageTitleMolecule from "../molecule/PageTitleMolecule";
+import { useAppState } from "../../hooks/useAppContext";
 
 /**
  * Represents the landing page component.
- * @returns {React.Component} - The rendered landing page component.
+ * @returns {JSX.Element} - The rendered landing page component.
  */
 const LandingPage = () => {
+  const state = useAppState();
+
+  const { userReducers } = state;
+
+  console.log("userReducers", userReducers);
+
   return (
     <>
       <PageTitleMolecule />
