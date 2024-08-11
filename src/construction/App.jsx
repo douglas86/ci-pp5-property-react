cbb1a20d0a407436b8f4ecea36cebca727102463
douglas <douglasmaxton@gmail.com>
2024-08-11 13:12:19 +0100

    feat: added console.log for profile data

--- src/construction/App.jsx
+++ src/construction/App.jsx
@@ -7,6 +7,7 @@ import useRefreshToken from "../hooks/useRefreshToken";
 import { useAppDispatch, useAppState } from "../hooks/useAppContext";
 
 import { router } from "../utils";
+import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -22,6 +23,8 @@ const App = () => {
   const { dataReducers } = state;
   const { showAlert } = dataReducers;
 
+  const heroku = "https://ci-pp5-property-api-958077e8a5b4.herokuapp.com";
+
   // custom hook that refreshes the auth tokens
   useRefreshToken();
 
@@ -32,6 +35,24 @@ const App = () => {
         dispatch({ type: "HIDE ALERT MESSAGE" });
       }, 5000);
   }, [showAlert, dispatch]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        return await axios.get(`${heroku}/profiles/3/`);
+      } catch (e) {
+        return e;
+      }
+    };
+
+    fetchData()
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }, []);
 
   return (
     <LayoutTemplate>
